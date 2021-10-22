16d2c1ad124d703b41943057246f7dc5acc22cb6
paulh <paulherbich1@gmail.com>
2021-10-22 23:22:36 +0200

    Landing Page rudimentary finished; Starting topic page;

--- hohenstein/src/components/webLandingPage.vue
+++ hohenstein/src/components/webLandingPage.vue
@@ -1,80 +1,139 @@
 <script>
+import webTopicRight from '@/components/webTopicRight.vue'
 export default {
   data() {
     return {
-      greeting: "Hello World!",
-    };
+      name: 'Hello World!'
+    }
+  },
+  
+  components: {
+    webTopicRight
   },
 };
 </script>
 
 <template>
-<div class="masterDiv">
-<div class="landingPage">
-  <nav>
-    <ul>
-      <li><a>Financial Advise</a></li>
-      <li><a>Real Estate</a></li>
-      <li><a>Classic Cars</a></li>
-      <li><a>Business Consulting</a></li>
-      <li><a>DE/EN/RU</a></li>
-    </ul>
-  </nav>
-  <div class=""> 
-      <img src="@/assets/mansion2.jpg" class="landingPageImage">
-  </div>
-  </div>
-  <div class="content">
-        <p> hello</p>
-  </div>
-  
+  <div class="masterDiv">
+    <div class="landingPage">
+      <nav>
+        <ul>
+          <li><a>Financial Advise</a></li>
+          <li><a>Real Estate</a></li>
+          <li><a>Classic Cars</a></li>
+          <li><a>Business Consulting</a></li>
+          <li><a>DE / EN / RU</a></li>
+        </ul>
+      </nav>
+      <div class="">
+        <img src="@/assets/mansion2.jpg" class="landingPageImage" />
+        <div class="whiteBox1">
+          <img
+            src="@/assets/HohensteinLogoText.png"
+            class="hohenSteinLogoText"
+          />
+          <p class="whiteBox1Slogan">Making Your Ideas Come True</p>
+          <div class="whiteBox1BlueLine"></div>
+        </div>
+      </div>
+   <a href="#content"> <img src="@/assets/arrow.png" class="landingPageArrow"> </a>
+    </div>
+    <div class="content" id="content">
+      <webTopicRight :name="'Classic Cars'" :slogan="'What a Luxury Car Should Be'"/>
+      
+    </div>
   </div>
 </template>
 
 <style>
-*{
-    font-family: Miller-Display;
+@import url(//db.onlinewebfonts.com/c/1fdf9a4d690c4e7271e535c39f40045f?family=Miller+Display);
+* {
+  font-family: "Miller Display";
 }
+html {
+  scroll-behavior: smooth;
+ }
 
-.landingPage{
-    position: absolute;
-    top:0;
-    width:90%;
-    height: 100%;
-    margin-left:5%;
-    margin-right:5%;
-    background-color:lightblue
+.landingPage {
+  position: absolute;
+  top: 0;
+  width: 90%;
+  height: 100%;
+  margin-left: 5%;
+  margin-right: 5%;
+ 
 }
-nav{
-    position: absolute;
-    top: 0;
-    text-align: center;
-    border: 1px solid black;
-    border-top: 0px;
-    border-left: 0px;
-    border-right: 0px;
-    height: 80px;
-    width: 100%;
+nav {
+  font-size:20px;
+  position: absolute;
+  top: 0;
+  text-align: center;
+  border: 1px solid black;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  height: 90px;
+  width: 100%;
 }
-ul{
-    list-style-type: none;
-    margin-top:30px;
-    padding:0;
+ul {
+  list-style-type: none;
+  margin-top: 40px;
+  padding: 0;
 }
-li{
-    display:inline;
-    margin:3%;
+li {
+  display: inline;
+  margin: 4%;
 }
-.content{
-    background-color: lightcoral;
-    height: 400px;
-    position: absolute;
-    top:100%;
+.content {
+  height: 1400px;
+  width: 90%;
+  margin-left: 5%;
+  margin-right: 5%;
+  position: absolute;
+  top: 100%;
+  background-color: gray;
 }
-.landingPageImage{
-    position: absolute;
-    top:20%;
-    right:0;
-    height: 500px;
+.landingPageImage {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 0;
+  width: 950px;
+}
+.whiteBox1 {
+  position: absolute;
+  height: 450px;
+  width: 500px;
+  background-color: white;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 700px;
+}
+.hohenSteinLogoText {
+  height: 200px;
+  transform: translate(-60%, 35%);
+  
+  position: absolute;
+}
+.whiteBox1BlueLine {
+  position: absolute;
+  top: 50%;
+  right: -75px;
+  height: 8px;
+  width: 150px;
+  background-color: #30375a;
+}
+.whiteBox1Slogan {
+  transform: translate(-8%,260px);
+  letter-spacing: 1px;
+  opacity: 70%;
+  font-size: 20px;
+}
+.landingPageArrow{
+  position: absolute;
+  height: 20px;
+  left: 15%;
+  bottom: 20px;
+
 }
 </style>
---
+++ hohenstein/src/components/webTopicRight.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      na9me: 'Hello World!'
+    }
+  },
+  props: {
+  name: String,
+  slogan: String
+}
+}
+</script>
+
+<template>
+  <div class="webTopic">
+  <div class="imageDiv">
+  </div>
+  <div class="whiteBox">
+      <p> {{ this.name }} </p>
+      <p> {{ this.slogan }} </p>
+  </div>
+  </div>
+</template>
+
+<style>
+.webTopic {
+  position: absolute;
+  height: 400px;
+  width: 100%;
+  transform: translateX(50%);
+  right:50%;
+  top: 80px;
+  background-color:red;
+}
+.whiteBox{
+    position: absolute;
+    height: 250px;
+    width: 750px;
+    background-color: orange;
+    transform: translateY(50%);
+    bottom:50%;
+}
+</style>
